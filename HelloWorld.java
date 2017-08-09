6dea5358fc9ae290f5cedaaa2ae5318e3a1fef2e
Nicole Bruck <nicole.a.bruck@gmail.com>
2017-08-09 15:37:14 -0700

    Initial commit

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+/** 
+ * A simple Java app that prints, "Hello, world!" to the console.
+ */
+public class HelloWorld {
+  public static void main(String[] args) {
+    System.out.println("Hello, World");
+  }
+}
